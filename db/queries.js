ce147dce7275b22c06dc397fd3d734c4b894f520
bscs24035 <bscs24035@itu.edu.pk>
2026-04-27 09:28:26 +0500

    implement query 1 signupUser

--- db/queries.js
+++ db/queries.js
@@ -44,8 +44,13 @@ const { ObjectId } = require('mongodb');
  * Hint: insertOne. Nothing fancy.
  */
 async function signupUser(db, userData) {
-  // TODO: implement
-  throw new Error('signupUser not implemented');
+  const result = await db.collection('users').insertOne({
+    email: userData.email,
+    passwordHash: userData.passwordHash,
+    name: userData.name,
+    createdAt: new Date()
+  });
+  return { insertedId: result.insertedId };
 }
 
 /**
